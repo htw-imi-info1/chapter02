b48488eeb8e5d5ca8754cd146d7b230e0cb6008c
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-04-22 13:44:57 +0200

    add implementation of Date

--- plain-java/Date.java
+++ plain-java/Date.java
@@ -1,4 +1,13 @@
 public class Date{
   private int day, month, year;
 
+  public Date(int year, int month, int day){
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+  public String format(){
+
+    return ""+day+"."+month+"."+year;
+  }
 }
