85b0e81135966d680e4b0741473320cdc6b87315
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-04-22 19:03:31 +0200

    solution polar point

--- point-exercise/PolarPoint.java
+++ point-exercise/PolarPoint.java
@@ -4,14 +4,19 @@
  */
 public class PolarPoint
 {
+    double radians;
+    double radius;
     public PolarPoint(int angle, double radius){
-      }
+        this.radians = Math.toRadians(angle); 
+        this.radius = radius;
+    }
     // see https://en.wikipedia.org/wiki/Polar_coordinate_system
     public int getX(){
-        return 0;
+        return (int)(radius * Math.cos(radians));
     }
+
     public int getY(){
-        return 0;
+        return (int)(radius * Math.sin(radians));
     }
-    
+
 }
