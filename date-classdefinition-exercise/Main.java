e5f0fc10da3dd0bd38067bfec56a0e05285586c5
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-04-22 16:15:22 +0200

    amend comments in Main

--- date-classdefinition-exercise/Main.java
+++ date-classdefinition-exercise/Main.java
@@ -1,9 +1,23 @@
 public class Main {
 
     public static void main(String[] args){
+        
+        // the Hello World - Example...
         System.out.println("Hello World!");
-        Date date = new Date(2022,4,22);
-        System.out.println(date.format());
+        
+        // write the code you wish you have:
+        // this is how we want to interact with dates - 
+        // the exercise is to create and fill the methods
+        // in class Date
+        // such that the this class compiles and the 
+        // program outputs the date in a 
+        // format like "22.4.2022". Start by uncommenting
+        // the following lines (one by one) which will
+        // result in a compile error - then define the 
+        // methods:
+        //
+        // Date date = new Date(2022,4,22);
+        // System.out.println(date.format());
     }
     
 }
