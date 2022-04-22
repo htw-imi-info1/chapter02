73cb0cea23a5aaf36b140677d8d1953e69816d68
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-04-22 19:14:38 +0200

    comment

--- point-exercise/PolarPoint.java
+++ point-exercise/PolarPoint.java
@@ -1,6 +1,9 @@
 /**
  * This is a point that takes polar coordinates for creation
- * and has getters for 
+ * and has getters for karthesian coordinates.
+ * Implement the constructor and the getters. Note that you
+ * need to decide on how you store the point internally.
+ * 
  */
 public class PolarPoint
 {
