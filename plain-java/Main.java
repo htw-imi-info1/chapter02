f890eee2a984dc64d60c7d60cd442ab8c9371e09
abductedRhino <avocadorable@mailbox.org>
2022-04-25 13:35:38 +0200

    Add more test dates

--- plain-java/Main.java
+++ plain-java/Main.java
@@ -4,5 +4,16 @@ public class Main {
     System.out.println("Hello World!");
     Date date = new Date(2022,4,22);
     System.out.println(date.format());
+    Date date = new Date (2022, 2, 28);
+    System.out.println(date.format());
+    Date leap = new Date(2000, 2, 7);
+    System.out.println(leap.format());
+    leap.setDay(29);
+    System.out.println(leap.format());
+    leap.setYear(2001);
+    System.out.println(leap.format());
+    Date test = new Date(-2000, 2, 8);
+    test.testMethodIsLeapYear();
+
   }
 }
