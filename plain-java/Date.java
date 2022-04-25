f53cd9dca19ae640a3df772c8687ccccd6cf3713
abductedRhino <104323196+abductedRhino@users.noreply.github.com>
2022-04-25 09:20:17 +0200

    I try GitHub and propose my Constructor.
    Updated Constructor to validate the date. It accounts for leap years and does not allow year 0 or below.

--- plain-java/Date.java
+++ plain-java/Date.java
@@ -1,11 +1,51 @@
 public class Date{
   private int day, month, year;
 
-  public Date(int year, int month, int day){
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  public Date(int year, int month, int day) {
+        if (isValidDate(year, month, day)) {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        } else {
+            System.out.println("Please enter a valid date.");
+        }
   }
+  
+  //Checks whether a date is plausible. Accounts for leap years.
+  private boolean isValidDate(int year, int month, int day) {
+        if (year > 0) {
+            if (month == 2) {
+                if ((isLeapYear(year) && 1 <= day && day <= 29)
+                        || (!isLeapYear(year) && 1 <= day && day <= 28)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if ((1 <= day && day <= 31 && month == 1 || month == 3 || month == 5
+                        || month == 7 || month == 8 || month == 10 || month == 12)
+                        || (1 <= day && day <= 30 && month == 4 || month == 6 || month == 9
+                        || month == 11)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+  }
+
+
+  //Checks whether a year is a leap year.
+  private boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+  }
+  
   public String format(){
 
     return ""+day+"."+month+"."+year;
